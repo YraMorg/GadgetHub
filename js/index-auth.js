198f5fd5f44bde163505ca0250c1eddde4b3c04c
Yura <90028629+YraMorg@users.noreply.github.com>
2025-05-13 18:19:45 +0300

    Update index-auth.js

--- js/index-auth.js
+++ js/index-auth.js
@@ -15,41 +15,21 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 // Вибираємо елементи
-const loginLink = document.getElementById("loginLink");
 const registerLink = document.getElementById("registerLink");
-const dashboardLink = document.getElementById("dashboardLink");
+const cartLink = document.getElementById("cartLink");
 const logoutBtn = document.getElementById("logoutBtn");
-const startNowBtn = document.getElementById("startNowBtn");
 
 onAuthStateChanged(auth, (user) => {
   console.log("Стан авторизації:", user); // Для перевірки
 
   if (user) {
-    if (loginLink) loginLink.style.display = "none";
     if (registerLink) registerLink.style.display = "none";
-    if (dashboardLink) dashboardLink.style.display = "inline-block";
+    if (cartLink) cartLink.style.display = "inline-block";
     if (logoutBtn) logoutBtn.style.display = "inline-block";
-
-    // Якщо авторизований — оновлюємо кнопку
-    if (startNowBtn) {
-      startNowBtn.textContent = "Знайти місця";
-      startNowBtn.onclick = () => {
-        window.location.href = "search.html";
-      };
-    }
   } else {
-    if (loginLink) loginLink.style.display = "inline-block";
     if (registerLink) registerLink.style.display = "inline-block";
-    if (dashboardLink) dashboardLink.style.display = "none";
+    if (cartLink) cartLink.style.display = "none";
     if (logoutBtn) logoutBtn.style.display = "none";
-
-    // Якщо неавторизований — повертаємо стандартний текст
-    if (startNowBtn) {
-      startNowBtn.textContent = "Почати зараз";
-      startNowBtn.onclick = () => {
-        window.location.href = "auth.html";
-      };
-    }
   }
 });
 
@@ -59,14 +39,5 @@ if (logoutBtn) {
     signOut(auth).then(() => {
       location.reload();
     });
-  });
-}
-
-// Кнопка "Увійти / Реєстрація"
-const authBtn = document.getElementById("authBtn");
-
-if (authBtn) {
-  authBtn.addEventListener("click", () => {
-    window.location.href = "auth.html";
   });
 }
