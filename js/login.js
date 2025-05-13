43a2164dd1da04c7db2cb4156f60be11728a9839
Yura <90028629+YraMorg@users.noreply.github.com>
2025-05-13 13:52:37 +0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,5 +1,18 @@
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-const auth = getAuth();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbv17e9KU79WSjkODDU0Hi_b9g2_vzPrk",
+  authDomain: "gadgethub-44258.firebaseapp.com",
+  projectId: "gadgethub-44258",
+  storageBucket: "gadgethub-44258.firebasestorage.app",
+  messagingSenderId: "231922939477",
+  appId: "1:231922939477:web:01f9436b11c2ff2a9b86dc"
+};
+
+// Ініціалізація Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export function loginUser() {
   const email = document.getElementById("login-email").value;
